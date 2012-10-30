247f336168156f4a747f01cdefd56217e737b952
Bich Le <bcle00@gmail.com>
2012-10-29 18:39:56 -0700

    Redesign key/value transformation disabling algorihtm using a data-driven approach.

--- parse_json.js
+++ parse_json.js
@@ -1,63 +1,98 @@
 
+// Determines whether key/value transformation should be disabled for the given key name.
+// Possible return types:
+// boolean, true: always disable transformation of both key and value
+// number: disable both if depth is less or equal to number
+// array: each element describes a set of conditions that must all be met to disable transformation.
+//     Each element is an object with the following optional properties indicating
+//     required conditions to disable transformation.
+//       maxDepth: depth must be less or equal to this number
+//       api: api name must match
+//       type: parent object must have a __type property that matches this string
+//     Additionally, the following optional properties determine additional behavior when disabling is true:
+//       recurse: recurse if the value is an object
+//       convert: if the value is a string, parse it as JSON and substitute
 var propNames = {
-  'objectId':1,
-  'createdAt':1,
-  'updatedAt':1,
-  '$nearSphere':1,
-  '$maxDistance':1,
-  'ACL':1,
-  '__type':1,
-  'iid':1,
-  'uuid':1,
-  'data':'follow',
-  'classname':1,
-  'className':1,
-  'v':1,
-  'includeRelationStub':1,
-  'include':'follow',
-  'limit':1,
-  'session_token':1,
-  '$in':1,
-  '$nin':1,
-  'commands':'follow',
-  'op':1,
-  'params':'follow',
-  'result':'follow',
-  // 'name':1,
-  'post_params':1,
-  'post_url':1,
-  'url':1,
-  'longitude':1,
-  'latitude':1
+  'objectId':true,
+  'createdAt':true,
+  'updatedAt':true,
+  '$nearSphere':true,
+  '$maxDistance':true,
+  'ACL':true,
+  '__type':true,
+  'iid':true,
+  'uuid':true,
+  'data': [{maxDepth:3, recurse:true, convert:true }],
+  'classname':true,
+  'className':true,
+  'v':true,
+  'includeRelationStub':true,
+  'include': [{maxDepth:1, api: 'find', recurse:true}],
+  'limit': [{maxDepth:1, api: 'find'}],
+  'session_token':true,
+  '$in':true,
+  '$nin':true,
+  'commands': [{maxDepth:1, api: 'multi', recurse:true}],
+  'op':true,
+  'params': [{maxDepth:2, api: 'multi', recurse:true}],
+  'result': [{maxDepth:1, recurse:true}],
+  'results': [{maxDepth:2, recurse:true}],
+  'name': [{maxDepth:2, api: 'upload_file'}, {type: 'File'}],
+  'post_params':true,
+  'post_url':true,
+  'url':true,
+  'username': [{maxDepth:3, api: 'user_login'}],
+  'email': [{maxDepth:3, api: 'user_login'}],  
+  'longitude':true,
+  'latitude':true
 };
 
-module.exports = function process_object (obj, transform, encoding) {
+
+function lookup(obj, key, api, depth) {
+  var entry = propNames[key];
+  if (!entry)
+    return null;
+  if (typeof entry === 'boolean')
+    return { recurse: false };
+  var result = null;
+  entry.forEach(function (el) {
+    if (result)
+      return; // already found a matching disabling condition
+    if (el.maxDepth && depth > el.maxDepth)
+      return;
+    if (el.api && api !== el.api)
+      return;
+    if (el.type && (!obj['__type'] || obj['__type'] != el.type))
+      return;
+    result = { recurse: !!el.recurse, convert: !!el.convert };
+  });
+  return result;
+}
+
+module.exports = function process_object (obj, transform, api, depth) {
   if (Array.isArray(obj)) {
     obj.forEach(function (element) {
       if (typeof element === 'object')
-        process_object(element, transform, encoding);
+        process_object(element, transform, api, depth);
     });
   } else for (var key in obj) {
-    var opc = propNames[key];
+    var opc = lookup(obj, key, api, depth);
     var val = obj[key];
-    if (!opc) {
-      var type = typeof val;
-      delete obj[key];
+    var recurse = true;
+    delete obj[key];
+    if (opc) { // disable key transformation
+      recurse = opc.recurse;
+      if (opc.convert && typeof val === 'string')
+        val = JSON.parse(val);
+    } else { // enable key transformation
       key = transform(key);
-      if (type === 'string') {
-	val = transform(val);
-      } else if (type === 'object') {
-        process_object(val, transform, encoding); 
-      }
-      obj[key] = val;
-    } else if (opc === 'follow') {
-      if (key === 'data' && typeof val === 'string')
-        obj[key] = val = JSON.parse(val);
-        
-      if (typeof val === 'string')
-        obj[key] = transform(val);
-      else
-        process_object(val, transform, encoding); 
     }
+    if (recurse) {
+      if (typeof val === 'string')
+        val = transform(val);
+      else if (typeof val === 'object')
+        process_object(val, transform, api, depth + 1); 
+    } 
+    obj[key] = val;
   }
 }
--- test.js
+++ test.js
@@ -1,17 +1,15 @@
 
 var fs = require('fs');
-var path = require('path');
 var process_object = require('./parse_json')
 
 
-// var path = path.join(__dirname, 'test', 'anywall_find_request_2.json');
-//var path = path.join(__dirname, 'test', 'anywall_create_request_conv.json');
 path = process.argv[2];
+api = process.argv[3];
 var input = fs.readFileSync(path, 'utf8');
 var obj = JSON.parse(input);
 console.log(JSON.stringify(obj, null, 2));
 console.log('----');
-process_object(obj, transform);
+process_object(obj, transform, api, 1);
 console.log(JSON.stringify(obj, null, 2));
 
 
